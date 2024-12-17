567cd0158f8312da0c7ee2aa972a92623587fffc
VelinaSouroujon <velinasouroujon@gmail.com>
2024-12-18 01:00:23 +0200

    Read maze map from file and print it on the console
    The maze map is represented as a matrix. initDefaultMatrix function initializes the matrix with default values. readMap function reads the map from a file and returns it as a matrix. printMatrix function prints the matrix. deleteMatrix function releases the dynamically allocated memory.

--- Maze Escape/Maze Escape.cpp
+++ Maze Escape/Maze Escape.cpp
@@ -1,9 +1,99 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+char** initDefaultMatrix(int rowCount, int colCount, char defaultSymbol)
+{
+    if (rowCount <= 0 || colCount <= 0)
+    {
+        return nullptr;
+    }
+
+    char** matrix = new char* [rowCount];
+
+    for (size_t i = 0; i < rowCount; i++)
+    {
+        matrix[i] = new char[colCount];
+
+        for (size_t j = 0; j < colCount; j++)
+        {
+            matrix[i][j] = defaultSymbol;
+        }
+    }
+
+    return matrix;
+}
+
+char** readMap(const char* mapPath, int rowCount, int colCount)
+{
+    if (mapPath == nullptr)
+    {
+        return nullptr;
+    }
+
+    std::ifstream map(mapPath);
+    if (!map.is_open())
+    {
+        return nullptr;
+    }
+
+    const char DEFAULT_SYMBOL = ' ';
+    char** matrix = initDefaultMatrix(rowCount, colCount + 1, DEFAULT_SYMBOL);
+
+    int rowIdx = 0;
+
+    while (rowIdx < rowCount
+        && map.getline(matrix[rowIdx], colCount + 1))
+    {
+        rowIdx++;
+    }
+
+    map.close();
+
+    return matrix;
+}
+
+void printMatrix(char** matrix, int rows, int cols)
+{
+    if (matrix == nullptr)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < cols; j++)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void deleteMatrix(char** matrix, int rows)
+{
+    if (matrix == nullptr)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const char mapPath[] = "../Maps/Level 1/Map 1.txt";
+    const int rows = 10;
+    const int cols = 15;
+
+    char** pMatrix = readMap(mapPath, rows, cols);
+    printMatrix(pMatrix, rows, cols);
+    deleteMatrix(pMatrix, rows);
 
     return 0;
 }
